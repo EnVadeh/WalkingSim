c6fa2da93dc8f4f287ee70f36785d58a36aab89e
EnVadeh <87571040+EnVadeh@users.noreply.github.com>
2025-01-17 23:27:12 +0545

    Building perlin noise calculator for the CPU

--- WalkingSim/source/chunkmanager.cpp
+++ WalkingSim/source/chunkmanager.cpp
@@ -100,46 +100,11 @@ void atmosphereLUTs::bind(GLuint shaderID, GLuint from, GLuint to) {
 }
 
 void atmosphereLUTs::createTransmittanceLUT() {
-	//Remnant from when I tried precomputing in the CPU FOR SOME REASON
-	//std::vector<float> data(TRANSMITTANCE_W * TRANSMITTANCE_H * 3);
-
-	//#pragma omp parallel for collapse(2) //nested fors(2) parallelised
-	//for (int j = 0; j < TRANSMITTANCE_H; j++)
-	//	for (int i = 0; i < TRANSMITTANCE_W; i++) {
-	//		float r, mu;
-	//		uvtoTransmittanceParams(float(i) / (TRANSMITTANCE_W - 1), float(j) / (TRANSMITTANCE_H - 1), r, mu);
-	//		
-	//		glm::vec3 transmittance = computeTransmittance(r, mu);
-	//		int idx = (j * TRANSMITTANCE_W + i) * 3;
-	//		data[idx + 0] = transmittance.x;
-	//		data[idx + 1] = transmittance.y;
-	//		data[idx + 2] = transmittance.z;
-	//	}
 	//CO[0].setup(TRANSMITTANCE_W, TRANSMITTANCE_H, 0);
 	CO[0].setup(1024, 1024, 0);
 }
 
 void atmosphereLUTs::createScatteringLUT() {
-	//std::vector<float> data(SCATTERING_R * SCATTERING_MU * SCATTERING_MU_S * SCATTERING_NU * 3);
-
-	//#pragma omp parallel for collapse(4)
-	//for(int l = 0; l < SCATTERING_NU; l++)
-	//	for(int k = 0; k < SCATTERING_MU_S; k++)
-	//		for(int j = 0; j < SCATTERING_MU; j++)
-	//			for (int i = 0; i < SCATTERING_R; i++) {
-	//				float r = atmosphere.earthRad + (atmosphere.atmosphereRad - atmosphere.earthRad) * float(i) / (SCATTERING_R - 1);
-	//				float mu = -1.0f + 2.0f * float(j) / (SCATTERING_MU - 1);
-	//				float muS = -1.0f + 2.0f * float(k) / (SCATTERING_MU_S - 1);
-	//				float nu = -1.0f + 2.0f * float(l) / (SCATTERING_NU - 1);
-	//				glm::vec3 scattering = computeScattering(r, mu, muS, nu);
-
-	//				int idx = ((l * SCATTERING_MU_S * SCATTERING_MU * SCATTERING_R + k * SCATTERING_MU * SCATTERING_R + j * SCATTERING_R + i) * 3);
-	//				
-	//				data[idx + 0] = scattering.x;
-	//				data[idx + 1] = scattering.y;
-	//				data[idx + 2] = scattering.z;
-
-	//			}
 
 	for (u8 i = 1; i < 4; i++)
 		CO[i].setup(SCATTERING_TEXTURE_WIDTH, SCATTERING_TEXTURE_HEIGHT, SCATTERING_TEXTURE_DEPTH, i); // 1 = scatteringLUT, 2 = rayleighDeltaLUT, 3 = mieDeltaLUT
@@ -149,82 +114,3 @@ void atmosphereLUTs::createScatteringLUT() {
 	CO[7].setup(SCATTERING_TEXTURE_WIDTH, SCATTERING_TEXTURE_HEIGHT, SCATTERING_TEXTURE_DEPTH, 7); // 7 = multiScatteringLUT
 
 }
-//
-//void atmosphereLUTs::uvtoTransmittanceParams(float u, float v, float& r, float& mu) {
-//	float H = sqrt(atmosphere.atmosphereRad * atmosphere.atmosphereRad - atmosphere.earthRad * atmosphere.earthRad);
-//	float rho = v * H;
-//	r = sqrt(rho * rho + atmosphere.earthRad * atmosphere.earthRad);
-//	float d_min = r - atmosphere.earthRad;
-//	float d_max = rho + H;
-//	float d = d_min + u * (d_max - d_min);
-//	mu = d == 0.0f ? 1.0f : (H * H - rho * rho - d * d) / (2.0f * r * d);
-//	mu = clamp(mu, -1.0f, 1.0f);
-//}
-//
-//glm::vec3 atmosphereLUTs::computeTransmittance(float r, float mu) {
-//	const int STEPS = 250;
-//	float dt = rayIntersectSphere(r, mu, atmosphere.atmosphereRad);
-//	dt /= float(STEPS);
-//
-//	glm::vec3 transmittance = glm::vec3(1.0f, 1.0f, 1.0f);
-//	float t = 0.0f;
-//
-//	for (int i = 0; i < STEPS; i++) {
-//		float r_sample = sqrt(r * r + t * t + 2.0f * r * mu * t);
-//		float height = r_sample - atmosphere.earthRad;
-//	
-//		glm::vec3 extinction = atmosphere.betaR * exp(-height / atmosphere.Hr) + atmosphere.betaM * exp(-height / atmosphere.Hm);
-//		transmittance *= exp(-extinction * dt);
-//		t += dt;
-//
-//	}
-//	return transmittance;
-//}
-//
-//glm::vec3 atmosphereLUTs::computeScattering(float r, float mu, float mu_s, float nu) {
-//	const int STEPS = 50;
-//	float dt = rayIntersectSphere(r, mu, atmosphere.atmosphereRad);
-//	dt /= float(STEPS);
-//
-//	glm::vec3 result = { 0.0f, 0.0f, 0.0f };
-//	float t = 0.0f;
-//
-//	for(int i = 0; i < STEPS; i++) {
-//		float r_sample = sqrt(r * r + t * t + 2.0 * r * mu * t);
-//		glm::vec3 pos = { 0.0f, r_sample, 0.0f };
-//		float height = r_sample - atmosphere.earthRad;
-//
-//		glm::vec3 rayleigh = atmosphere.betaR * exp(-height/ atmosphere.Hr);
-//		glm::vec3 mie = atmosphere.betaM * exp(-height/ atmosphere.Hm);
-//	
-//		float cos_theta = mu * mu_s + sqrt((1.0f - mu * mu) * (1.0f - mu_s * mu_s)) * nu;
-//		float rayleigh_phase = 3.0f / (16.0f * M_PI) * (1.0f + cos_theta * cos_theta);
-//
-//		float g = atmosphere.meiG;
-//
-//		float mie_phase = 3.0f / (8.0f * M_PI) * ((1.0f - g * g) * (1.0f + cos_theta * cos_theta)) / ((2.0f + g * g) * pow(1.0f + g * g - 2.0f * g * cos_theta, 1.5f));
-//
-//		glm::vec3 sun_transmittance = computeTransmittance(r_sample, mu_s);
-//		glm::vec3 view_transmittance = computeTransmittance(r_sample, -mu);
-//
-//		result += (rayleigh * rayleigh_phase + mie * mie_phase) * sun_transmittance * view_transmittance * dt;
-//
-//		t += dt;
-//	}
-//	return result;
-//}
-//
-//float atmosphereLUTs::rayIntersectSphere(float r, float mu, float radius) {
-//	float b = 2.0f * r * mu;
-//	float c = r * r - radius * radius;
-//
-//	float det = b * b - 4.0f * c;
-//
-//	if (det < 0.0f) return 0.0f;
-//	det = sqrt(det);
-//
-//	float t1 = (-b + det) * 0.5f;
-//	float t2 = (-b - det) * 0.5f;
-//
-//	return (t1 >= 0.0f) ? t1 : t2;
-//}
