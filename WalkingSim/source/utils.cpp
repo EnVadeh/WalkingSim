63c880aaa8a6f36a5a94991eaf68c6903a82b142
EnVadeh <87571040+EnVadeh@users.noreply.github.com>
2025-01-18 17:59:11 +0545

    added definition of the gradient function as I was getting error for defining it in the header

--- WalkingSim/source/utils.cpp
+++ WalkingSim/source/utils.cpp
@@ -9,7 +9,19 @@ std::vector <GLuint> SHADERS;
 float deltaTime = 0.0f; // Time between current frame and last frame
 float lastFrame = 0.0f;
 
+vector2D randomGradient(vector2D p) {
+	p = p + 0.1;
+	float x = dotProduct(p, vector2D(123.4, 234.5));
+	float y = dotProduct(p, vector2D(234.5, 345.6));
+	vector2D gradient = vector2D(x, y);
+	gradient.sinof();
+	gradient = gradient * 43758.5453;
 
+	gradient.sinof();
+	return gradient;
+}
+
+double mix(double x, double y, double a) { return (x * (1 - a) + y * a); }
 
 glm::mat4 createGeometricToWorldMatrix(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale) {
 	glm::mat4 Scale = glm::scale(glm::mat4(1.0f), scale);
