c55d31a1dd0a0d35b3e74fd99953e9f9d36b9faf
EnVadeh <87571040+EnVadeh@users.noreply.github.com>
2025-01-30 17:44:10 +0545

    lights generate matrix and send to UBO now, also need to make it more customizable. read comments

--- WalkingSim/source/light.cpp
+++ WalkingSim/source/light.cpp
@@ -5,14 +5,23 @@ void lightManager::initLight(glm::vec4 lightPos, glm::vec4 lightDir, glm::vec4 l
 	temp.vLightPos = lightPos;
 	temp.vLightDir = lightDir;
 	temp.vLightColor = lightColor;
+	glm::vec3 vRight = glm::normalize(glm::cross(glm::vec3(lightDir), glm::vec3(0, 1, 0)));
+	glm::vec3 vUp = glm::normalize(glm::cross(vRight, glm::vec3(lightDir)));
+	temp.mView = glm::lookAt(glm::vec3(lightPos), glm::vec3(lightDir), vUp);
+	temp.mProjection = glm::perspective(float(glm::radians(60.0f)), 1.0f, 0.1f, 10000.0f);
 	temp.isOn = false;
 	temp.type = lightType::pointLight;
 	lights.push_back(temp);
 	LIGHT_COUNT++;
 }
 
-void lightManager::initLight(glm::vec4 lightDir, glm::vec4 lightColor) {
+void lightManager::initLight(glm::vec4 lightDir, glm::vec4 lightColor) { //I'd love to take a distance factor for lightPosition fake, and boundary values
 	light temp;
+	glm::vec3 lightPos = -glm::vec3(lightDir) * 5.0f;
+	glm::vec3 vRight = glm::normalize(glm::cross(glm::vec3(lightDir), glm::vec3(0, 1, 0)));
+	glm::vec3 vUp = glm::normalize(glm::cross(vRight, glm::vec3(lightDir)));
+	temp.mView = glm::lookAt(glm::vec3(lightPos), glm::vec3(lightDir), glm::vec3(0, 1, 0));
+	temp.mProjection = glm::ortho(-5.f, 5.f, -5.f, 5.f, -10000.1f, 10000.0f);
 	temp.vLightDir = lightDir;
 	temp.vLightColor = lightColor;
 	temp.isOn = false;
@@ -23,6 +32,10 @@ void lightManager::initLight(glm::vec4 lightDir, glm::vec4 lightColor) {
 
 void lightManager::initLight(glm::vec4 lightPos, glm::vec4 lightDir, glm::vec4 lightColor, glm::vec4 epA, glm::vec4 epB) {
 	light temp;
+
+	//I haven't figured out what to do with Area light Source
+	//glm::vec3 vRight = glm::normalize(glm::cross(glm::vec3(lightDir), glm::vec3(0, 1, 0)));
+	//glm::vec3 vUp = glm::normalize(glm::cross(vRight, glm::vec3(lightDir)));
 	temp.vLightPos = lightPos;
 	temp.vLightDir = lightDir;
 	temp.vLightColor = lightColor;
@@ -67,7 +80,7 @@ void lightManager::turnOff(unsigned int from, unsigned int to) {
 void lightManager::setLights() {
 	for (const auto& light : lights)
 		if (light.isOn)
-			lightData.push_back({ light.vLightPos, light.vLightDir, light.vLightColor, light.vEndPointA, light.vEndPointB });
+			lightData.push_back({ light.vLightPos, light.vLightDir, light.vLightColor, light.vEndPointA, light.vEndPointB, light.mProjection, light.mView });
 
 	uniformBuffer<lightUBOData> lightBuff(lightData, drawFreq::staticDraw);
 	lightBuff.bind();
